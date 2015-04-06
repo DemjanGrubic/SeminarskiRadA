734af0b8d33116a30d5ba04c6682ac9e6c50617a
DemjanGrubic <demjangrubic@gmail.com>
2015-04-06 21:48:35 +0200

    new files

---
+++ TotalCommander/DirectoryListItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TotalCommander
+{
+    class DirectoryListItem
+    {
+        public FileSystemInfo FileSystemInfo { get; set; }
+        public ImageSource ImageSource { get; set; }
+
+        public DirectoryListItem(string path, bool isDirectory)
+        {
+            if (isDirectory)
+            {
+                FileSystemInfo = new DirectoryInfo(path);
+                ImageSource = IconExtractor.GetIcon(path, true, true);
+            }
+            else
+            {
+                FileSystemInfo = new FileInfo(path);
+                ImageSource = IconExtractor.GetIcon(path, true, false);
+            }
+        }
+    }
+}
